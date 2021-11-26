bf606c69b0d073f89a07896c1449b2076b5a7a00
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-11-27 00:08:29 +0200

    feat: add get-by-id response

--- src/routing/getResponse.js
+++ src/routing/getResponse.js
@@ -2,13 +2,23 @@ import { db } from '../db.js';
 
 const getResponse = (pathParts, res) => {
   if (pathParts.length === 1) {
-    console.log(pathParts);
     try {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(db));
     } catch (error) {
       res.writeHead(500, { 'Content-Type': 'application/json' });
       res.end('Something is wrong');
+    }
+  } else {
+    if (pathParts.length === 2) {
+      try {
+          const user = db.find(item => item.id === pathParts[1])
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+      } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end('Something is wrong');
+      }
     }
   }
 };
